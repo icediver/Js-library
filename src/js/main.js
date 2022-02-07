f604d7b653a4090a261e5dab309262b7743c958e
IceDiver <admin@example.com>
2022-02-07 11:52:06 +0300

    Add html, eq, index, find, closest, siblings func

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 import './lib/lib';
 
 $('button').on('click', function() {
-  $(this).toggleClass('active')
+  $('div').eq(2).toggleClass('active')
 });
-
+$('div').click(function() {
+  console.log($(this).index());
+})
+// console.log($('div').eq(2).find('.more'));
+// console.log($('.some').closest('.findmeq').addClass('qeere'));
+// console.log($('button').html());
+console.log($('.findme').siblings());
