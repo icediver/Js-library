2e82b83c7aa7a38f75d2b96de4450bcacfc5373d
IceDiver <admin@example.com>
2022-02-10 01:35:58 +0300

    Add fadeIn fadeOut functions

--- src/js/main.js
+++ src/js/main.js
@@ -9,4 +9,5 @@ $('div').click(function() {
 // console.log($('div').eq(2).find('.more'));
 // console.log($('.some').closest('.findmeq').addClass('qeere'));
 // console.log($('button').html());
-console.log($('.findme').siblings());
+// $('.findme').fadeOut(1800);
+$('button').fadeIn(1800);
