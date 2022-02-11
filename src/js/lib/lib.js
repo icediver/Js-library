0d43c163724bcdbaa5b66122cba8ef63cb111015
IceDiver <admin@example.com>
2022-02-11 08:20:41 +0300

    Add collapse element

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -5,7 +5,8 @@ import './modules/actions';
 import './modules/handlers';
 import './modules/effects';
 import './components/dropdown';
-import './components/modal'
-import './components/tab'
+import './components/modal';
+import './components/tab';
+import './components/accordion';
 
 export default $;
